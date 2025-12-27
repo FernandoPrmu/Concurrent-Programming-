be18a4ec47bd227588091f00f18a725562333a10
Pramuditha Fernando <152087363+FernandoPrmu@users.noreply.github.com>
2025-12-27 17:25:33 +0530

    create student class
    create student class in old system

---
+++ Concurrent_Programming_CW/src/Student.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+public class Student {
+    private int Student_Id;
+    private String name;
+    private Random random;
+
+    public Student(int student_Id, String name, Random random) {
+        Student_Id = student_Id;
+        this.name = name;
+        this.random = random;
+    }
+
+    public int getStudent_Id() {
+        return Student_Id;
+    }
+
+    public void setStudent_Id(int student_Id) {
+        Student_Id = student_Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Random getRandom() {
+        return random;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
+
+    public String submitExam() throws InterruptedException {
+        int processTime = random.nextInt(100);
+        Thread.sleep(processTime);
+        int resultTime = random.nextInt(100);
+        if(resultTime > 10){
+            return "Successfully Submitted";
+        }else{
+            return "Failure Timeout";
+        }
+    }
+
+}
