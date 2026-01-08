aefcd4d592eedce128134d3ca24fc542fe00c93c
Pramuditha Fernando <152087363+FernandoPrmu@users.noreply.github.com>
2026-01-08 11:01:06 +0530

    Name adding

--- Concurrent_Programming_CW/src/New_version/ExamSubmissionSimulator.java
+++ Concurrent_Programming_CW/src/New_version/ExamSubmissionSimulator.java
@@ -2,7 +2,7 @@ package New_version;
 
 public class ExamSubmissionSimulator {
     public static void main(String[] args) throws InterruptedException {
-        int numberOfStudents = 100000;  // Total number of students attempting to submit the exam
+        int numberOfStudents = 5000;  // Total number of students attempting to submit the exam
         int poolSize = Runtime.getRuntime().availableProcessors()*2; // Thread pool size based on available CPU cores (optimized for performance)
         NewSubmissionSystem system = new NewSubmissionSystem(numberOfStudents,poolSize); // Create the submission system
         system.processSubmissions(); // Start processing all student submissions
